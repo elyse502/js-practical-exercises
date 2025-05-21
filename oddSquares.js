82c41423bae98b9b9dfd5bf8301069fc0cb223ab
elyse502 <elyseniyibizi502@gmail.com>
2025-05-21 17:22:18 +0300

    Implement oddSquares function to filter odd numbers and return their squares

---
+++ oddSquares.js
@@ -0,0 +1,3 @@
+exports.oddSquares = function (arr) {
+    return arr.filter(num => num % 2 !== 0).map(num => num ** 2);
+};
---
+++ tests/oddSquares-main.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+const oddSquares = require("../oddSquares").oddSquares;
+console.log(oddSquares([1, 2, 3, 4, 5, 6]));
+console.log(oddSquares([10, 15, 20, 25, 30]));
